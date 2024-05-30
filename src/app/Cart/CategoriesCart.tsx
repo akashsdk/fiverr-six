4e1cf2cceacea95ca5d17e4f3fe7315de2374df6
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-30 17:19:46 +0600

    ok

--- src/app/Cart/CategoriesCart.tsx
+++ src/app/Cart/CategoriesCart.tsx
@@ -1,10 +1,20 @@
 "use client";
-import React from 'react'
 
-function CategoriesCart() {
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
+
+interface CategoriesCartProps {
+  text: string;
+  img: StaticImageData;
+}
+
+const CategoriesCart: React.FC<CategoriesCartProps> = ({ text, img }) => {
   return (
-    <div>CategoriesCart</div>
-  )
+    <div className='h-[180px] w-[150px] flex flex-col justify-center items-center rounded-[5px] m-2 p-[5px] border-2 cursor-pointer md:hover:shadow-lg'>
+      <Image alt='cart' src={img} width={150} height={150} />
+      <p className='text-center text-[20px] mt-2 opacity-90 text-red-500'>{text}</p>
+    </div>
+  );
 }
 
 export default CategoriesCart;
--- src/app/Screen/Categories.tsx
+++ src/app/Screen/Categories.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import CategoriesCart from "../Cart/CategoriesCart";
 
+import Img1 from "../../Data/Img/Categories-1.png";
+import Img2 from "../../Data/Img/Categories-2.png";
+
 export default function Categories() {
   return (
     <div className="w-full flex justify-center items-center mt-[20px] md:mt-[50px]">
@@ -9,8 +12,9 @@ export default function Categories() {
         <p className="md:text-[35px] text-[25px]">Featured Categories</p>
         <p className="md:text-[20px] opacity-60">Get your desired product from featured category</p>
 
-        <div>
-            <CategoriesCart />
+        <div className="flex">
+            <CategoriesCart img={Img1} text="Personal Care" />
+            <CategoriesCart img={Img2} text="Chocolate"/>
         </div>
       </div>
     </div>
