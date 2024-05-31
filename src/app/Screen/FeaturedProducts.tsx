54a2e9c7f344b4761e7082d9784e121a5012e617
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-31 20:42:57 +0600

    Update FeaturedProducts.tsx

--- src/app/Screen/FeaturedProducts.tsx
+++ src/app/Screen/FeaturedProducts.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
 
 export default function FeaturedProducts() {
   return (
-    <div>FeaturedProducts</div>
-  )
+    <div className="w-full flex justify-center items-center mt-[20px] md:mt-[50px]">
+      <div className="Categories-Box w-11/12">
+        <p className="md:text-[35px] text-[25px] text-center">
+          Featured Products
+        </p>
+        <div>
+            <button>BEST DEALS</button>
+            <button>BEST SELLERS</button>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
