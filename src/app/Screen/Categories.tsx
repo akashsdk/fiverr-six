42eb542419b0539fc0e2d56585c209f199e67dac
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-31 11:19:01 +0600

    gfhgc

--- src/app/Screen/Categories.tsx
+++ src/app/Screen/Categories.tsx
@@ -13,26 +13,30 @@ import Img7 from "../../Data/Img/Categories-7.png";
 export default function Categories() {
   return (
     <div className="w-full flex justify-center items-center mt-[20px] md:mt-[50px]">
-      <div className="Categories-Box">
-        <p className="md:text-[35px] text-[25px]">Featured Categories</p>
-        <p className="md:text-[20px] opacity-60">Get your desired product from featured category</p>
+      <div className="Categories-Box w-11/12">
+        <p className="md:text-[35px] text-[25px] text-center">
+          Featured Categories
+        </p>
+        <p className="md:text-[20px] text-[16px] opacity-60 text-center mb-4">
+          Get your desired product from featured category
+        </p>
 
-        <div className="flex">
-            <CategoriesCart img={Img1} text="Personal Care" />
-            <CategoriesCart img={Img2} text="Chocolate"/>
-            <CategoriesCart img={Img3} text="Silk Saree" />
-            <CategoriesCart img={Img4} text="Poster"/>
-            <CategoriesCart img={Img5} text="T-Shirts - Woman"/>
-            <CategoriesCart img={Img6} text="T-Shirts - Men"/>
-            <CategoriesCart img={Img7} text="Cotton Cap"/>
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 justify-items-center">
+          <CategoriesCart img={Img1} text="Personal Care" />
+          <CategoriesCart img={Img2} text="Chocolate" />
+          <CategoriesCart img={Img3} text="Silk Saree" />
+          <CategoriesCart img={Img4} text="Poster" />
+          <CategoriesCart img={Img5} text="T-Shirts - Woman" />
+          <CategoriesCart img={Img6} text="T-Shirts - Men" />
+          <CategoriesCart img={Img7} text="Cotton Cap" />
 
-            <CategoriesCart img={Img1} text="Personal Care 2" />
-            <CategoriesCart img={Img2} text="Chocolate 2"/>
-            <CategoriesCart img={Img3} text="Silk Saree 2" />
-            <CategoriesCart img={Img4} text="Poster 2"/>
-            <CategoriesCart img={Img5} text="T-Shirts - Woman 2"/>
-            <CategoriesCart img={Img6} text="T-Shirts - Men 2"/>
-            <CategoriesCart img={Img7} text="Cotton Cap 2"/>
+          <CategoriesCart img={Img1} text="Personal Care 2" />
+          <CategoriesCart img={Img2} text="Chocolate 2" />
+          <CategoriesCart img={Img3} text="Silk Saree 2" />
+          <CategoriesCart img={Img4} text="Poster 2" />
+          <CategoriesCart img={Img5} text="T-Shirts - Woman 2" />
+          <CategoriesCart img={Img6} text="T-Shirts - Men 2" />
+          <CategoriesCart img={Img7} text="Cotton Cap 2" />
         </div>
       </div>
     </div>
