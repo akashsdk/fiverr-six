c975378b9d6f05c4e23558538ce81560ceb57d50
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-02 19:53:30 +0600

    ok

--- src/app/Screen/FeaturedProducts.tsx
+++ src/app/Screen/FeaturedProducts.tsx
@@ -21,15 +21,11 @@ export default function FeaturedProducts() {
   return (
     <div className="w-full flex justify-center items-center mt-[20px] md:mt-[50px]">
       <div className="Categories-Box w-11/12 text-center">
-        <p className="md:text-[35px] text-[25px] text-center">
-          Featured Products
-        </p>
+        <p className="md:text-[35px] text-[25px] text-center">Featured Products</p>
         <div className="flex justify-center my-4">
           <button
             onClick={() => handlePageChange(1)}
-            className={`mx-2 px-4 py-2 relative transition-colors duration-500 ${
-              page === 1 ? "text-red-500" : "text-gray-500"
-            }`}
+            className={`mx-2 px-4 py-2 relative transition-colors duration-500 ${page === 1 ? "text-red-500" : "text-gray-500"}`}
           >
             BEST DEALS
             {page === 1 && (
@@ -38,9 +34,7 @@ export default function FeaturedProducts() {
           </button>
           <button
             onClick={() => handlePageChange(2)}
-            className={`mx-2 px-4 py-2 relative transition-colors duration-500 ${
-              page === 2 ? "text-red-500" : "text-gray-500"
-            }`}
+            className={`mx-2 px-4 py-2 relative transition-colors duration-500 ${page === 2 ? "text-red-500" : "text-gray-500"}`}
           >
             BEST SELLERS
             {page === 2 && (
@@ -49,138 +43,125 @@ export default function FeaturedProducts() {
           </button>
         </div>
 
-        <div
-          className={`transition-opacity duration-500 ${
-            fade ? "opacity-0" : "opacity-100"
-          }`}
-          style={{ width: "100%" }}
-        >
+        <div className={`transition-opacity duration-500 ${fade ? "opacity-0" : "opacity-100"}`} style={{ width: '100%' }}>
           {page === 1 ? (
-            <div className="FeaturedProducts-Box1">
-              <div className="FeaturedProducts-Box2">
-                <div className="FeaturedProducts-Div">
-                  <ProductCart
-                    img={Img1}
-                    img2={Img2}
-                    img3={Img3}
-                    type="Poster"
-                    text="Poster 1"
-                    price="$23.50"
-                    discountPercentage="2"
-                    details="Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details"
-                    status="Out Stock"
-                    sells="20"
-                    rating={3}
-                    code="Pos-1"
-                  />
-                  <ProductCart
-                    img={Img1}
-                    img2={Img2}
-                    img3={Img3}
-                    type="Poster"
-                    text="Poster 2"
-                    price="$23.50"
-                    discountPercentage="2"
-                    details="Product 1 "
-                    status="Out Stock"
-                    sells="20"
-                    rating={3}
-                    code="Pos-1"
-                  />
-                </div>
-                <div className="FeaturedProducts-Div">
-                  <ProductCart
-                    img={Img1}
-                    img2={Img2}
-                    img3={Img3}
-                    type="Poster"
-                    text="Poster 3"
-                    price="$23.50"
-                    discountPercentage="2"
-                    details="Product 1 "
-                    status="Out Stock"
-                    sells="20"
-                    rating={3}
-                    code="Pos-1"
-                  />
-                  <ProductCart
-                    img={Img1}
-                    img2={Img2}
-                    img3={Img3}
-                    type="Poster"
-                    text="Poster 4"
-                    price="$23.50"
-                    discountPercentage="2"
-                    details="Product 1 Details P Product 1 Details"
-                    status="In Stock"
-                    sells="20"
-                    rating={3}
-                    code="Pos-1"
-                  />
-                </div>
-              </div>
-              <div className="flex">
-                <ProductCart
-                  img={Img1}
-                  img2={Img2}
-                  img3={Img3}
-                  type="Poster"
-                  text="Poster 1"
-                  price="$23.50"
-                  discountPercentage="2"
-                  details="Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details"
-                  status="Out Stock"
-                  sells="20"
-                  rating={3}
-                  code="Pos-1"
-                />
-                <ProductCart
-                  img={Img1}
-                  img2={Img2}
-                  img3={Img3}
-                  type="Poster"
-                  text="Poster 1"
-                  price="$23.50"
-                  discountPercentage="2"
-                  details="Product 1 "
-                  status="Out Stock"
-                  sells="20"
-                  rating={3}
-                  code="Pos-1"
-                />
-                <ProductCart
-                  img={Img1}
-                  img2={Img2}
-                  img3={Img3}
-                  type="Poster"
-                  text="Poster 1"
-                  price="$23.50"
-                  discountPercentage="2"
-                  details="Product 1 "
-                  status="Out Stock"
-                  sells="20"
-                  rating={3}
-                  code="Pos-1"
-                />
-                <ProductCart
-                  img={Img1}
-                  img2={Img2}
-                  img3={Img3}
-                  type="Poster"
-                  text="Poster 1"
-                  price="$23.50"
-                  discountPercentage="2"
-                  details="Product 1 Details P Product 1 Details"
-                  status="In Stock"
-                  sells="20"
-                  rating={3}
-                  code="Pos-1"
-                />
-              </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details"
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 "
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 "
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 Details P Product 1 Details"
+                status="In Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details Product 1 Details"
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 "
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 "
+                status="Out Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
+              <ProductCart
+                img={Img1}
+                img2={Img2}
+                img3={Img3}
+                type="Poster"
+                text="Poster 1"
+                price="$23.50"
+                discountPercentage="2"
+                details="Product 1 Details P Product 1 Details"
+                status="In Stock"
+                sells="20"
+                rating={3}
+                code="Pos-1"
+              />
             </div>
           ) : page === 2 ? (
             <div>
-              <p> page 2</p>
+              <p>Page 2</p>
             </div>
           ) : (
             <h1>Error page</h1>
