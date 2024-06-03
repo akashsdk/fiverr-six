7b2154f80741ecfc05c8f7ce5bfd0581de0644e9
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-04 02:36:38 +0600

    ok

--- src/app/Screen/ShortIndex.tsx
+++ src/app/Screen/ShortIndex.tsx
@@ -1,18 +1,43 @@
-"use client";
-import Image from 'next/image';
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
-import Icon1 from "../../Data/Icon/shield.png";
+import Icon1 from "../../Data/Icon/delivery-man.png";
+import Icon2 from "../../Data/Icon/return.png";
+import Icon3 from "../../Data/Icon/100-dollar.png";
+import Icon4 from "../../Data/Icon/shield.png";
+import Icon5 from "../../Data/Icon/original.png";
 
 export default function ShortIndex() {
   return (
-    <div className="web-bg2 w-full h-[50px] flex justify-center items-center">
-        <div className="w-11/12 flex justify-center items-center">
-            <div>
-                <Image alt='ok' src={Icon1} />
-                <p>Safe Payments</p>
-            </div>
+    <div className="web-bg2 w-full flex justify-center items-center">
+      <div className="w-full max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="h-[50px] flex justify-center items-center mb-2 sm:mb-0">
+            <Image alt="Nationwide Delivery" src={Icon1} height={30} />
+            <p className="ml-2 md:mr-2 sm:ml-4">Nationwide Delivery |</p>
+          </div>
+
+          <div className="h-[50px] flex justify-center items-center mb-2 sm:mb-0">
+            <Image alt="Free & Easy Return" src={Icon2} height={30} />
+            <p className="ml-2 md:mr-2 sm:ml-4">Free & Easy Return |</p>
+          </div>
+
+          <div className="h-[50px] flex justify-center items-center mb-2 sm:mb-0">
+            <Image alt="Best Price Guaranteed" src={Icon3} height={30} />
+            <p className="ml-2 md:mr-2 sm:ml-4">Best Price Guaranteed |</p>
+          </div>
+
+          <div className="h-[50px] flex justify-center items-center mb-2 sm:mb-0">
+            <Image alt="Safe Payments" src={Icon4} height={30} />
+            <p className="ml-2 md:mr-2 sm:ml-4">Safe Payments |</p>
+          </div>
+
+          <div className="h-[50px] flex justify-center items-center">
+            <Image alt="100% Authentic Products" src={Icon5} height={30} />
+            <p className="ml-2 md:mr-2 sm:ml-4">100% Authentic Products</p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
