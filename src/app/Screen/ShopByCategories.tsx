4ccfd63992c489c993497d644a94859f78f9e8c1
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-04 00:53:50 +0600

    Update ShopByCategories.tsx

--- src/app/Screen/ShopByCategories.tsx
+++ src/app/Screen/ShopByCategories.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import Image, { StaticImageData } from 'next/image'; 
+
+import IMG1 from "../../Data/Img/Product-1.png";
+import IMG2 from "../../Data/Img/Product-2.png";
+
+interface SBCcomponentProps {
+  img: StaticImageData; 
+  text: string;
+}
+
+const SBCcomponent: React.FC<SBCcomponentProps> = ({ img, text }) => {
+  return (
+    <div className="text-center m-4">
+      <Image alt="cart" src={img} className="w-full h-auto" />
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default function ShopByCategories() {
   return (
-    <div>ShopByCategories</div>
-  )
+    <div className="w-full flex justify-center items-center mt-[20px] md:mt-[50px]">
+      <div className="w-11/12">
+        <p className="md:text-[35px] text-[25px] text-center">Shop By Categories</p>
+
+        <div className='flex justify-center items-center'>
+          <SBCcomponent img={IMG1} text="Product 1" />
+          <SBCcomponent img={IMG2} text="Product 2" />
+        </div>
+      </div>
+    </div>
+  );
 }
