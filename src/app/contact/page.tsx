6820371fbcd30b48dec2ccf9b21f01b89a49d3d1
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-05 01:08:52 +0600

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,74 +1,113 @@
 "use client";
-import Image from 'next/image';
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
-export default function Contact() {
+import {
+  MailOutlined,
+  PhoneOutlined,
+  EnvironmentOutlined,
+  FacebookOutlined,
+  WhatsAppOutlined,
+  YoutubeOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
+
+const Contact: React.FC = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="w-11/12 md:mt-[20px] md:mb-[30px]">
-        <div className='flex flex-col md:flex-row justify-center items-start'>
-          <div className='w-full md:w-1/2 p-4'>
-            <p className="md:text-[35px] text-[25px] font-bold mb-4">Get In Touch</p>
-            <form className='flex flex-col space-y-4'>
+        <div className="flex flex-col md:flex-row justify-center items-start">
+          <div className="w-full md:w-1/2 p-4">
+            <p className="md:text-[35px] text-[25px] font-bold mb-4 text-green-700">
+              Get In Touch
+            </p>
+            <form className="flex flex-col space-y-4">
               <input
                 type="text"
-                placeholder='Full name'
-                className='border p-2 w-full'
+                placeholder="Full name"
+                className="border-2 p-2 w-full  rounded-[5px] outline-none"
               />
               <input
                 type="text"
-                placeholder='Phone Number'
-                className='border p-2 w-full'
+                placeholder="Phone Number"
+                className="border p-2 w-full  rounded-[5px] outline-none"
               />
               <input
                 type="email"
-                placeholder='Email'
-                className='border p-2 w-full'
+                placeholder="Email"
+                className="border p-2 w-full rounded-[5px] outline-none"
               />
               <textarea
-                placeholder='Message'
-                className='border p-2 w-full'
-                rows="4"
+                placeholder="Message"
+                className="border p-2 w-full rounded-[5px] outline-none"
+                rows={4}
               />
-              <button type="submit" className="bg-green-500 text-white p-2 rounded">
+              <button
+                type="submit"
+                className="bg-green-700 text-white p-2 rounded"
+              >
                 SEND NOW
               </button>
             </form>
           </div>
-          <div className='w-full md:w-1/2 p-4 flex flex-col space-y-4'>
-            <p className="md:text-[35px] text-[25px] font-bold mb-4">Talk To Us</p>
-            <div className='flex items-center'>
-              <Image src="/path/to/email-icon.png" alt="Email" className='mr-2' />
-              <div>
-                <p className='font-bold'>EMAIL</p>
-                <p>something@tyler.com</p>
+
+          <div className="w-full md:w-1/2 p-4 flex flex-col space-y-4">
+            <p className="md:text-[35px] md:ml-3 text-[25px] font-bold mb-4 text-green-700">
+              Talk To Us
+            </p>
+            <div className="flex items-center md:ml-3">
+              <div className="h-[50px] w-[50px] rounded-[25px] border-2 flex justify-center items-center">
+                <MailOutlined className="text-[25px] opacity-70" />
+              </div>
+              <div className="ml-5">
+                <p className="font-bold">EMAIL</p>
+                <p className="opacity-70">something@tyler.com</p>
               </div>
             </div>
-            <div className='flex items-center'>
-              <Image src="/path/to/phone-icon.png" alt="Phone Number" className='mr-2' />
-              <div>
-                <p className='font-bold'>PHONE NUMBER</p>
-                <p>202-555-0188</p>
+            <div className="flex items-center md:ml-3">
+              <div className="h-[50px] w-[50px] rounded-[25px] border-2 flex justify-center items-center">
+                <PhoneOutlined className="text-[25px] opacity-70" />
+              </div>
+              <div className="ml-5">
+                <p className="font-bold">PHONE NUMBER</p>
+                <p className="opacity-70">202-555-0188</p>
               </div>
             </div>
-            <div className='flex items-center'>
-              <Image src="/path/to/address-icon.png" alt="Address" className='mr-2' />
-              <div>
-                <p className='font-bold'>ADDRESS</p>
-                <p>2727 Ocean Road, Malibu, CA, 90264</p>
+            <div className="flex items-center md:ml-3">
+              <div className="h-[50px] w-[50px] rounded-[25px] border-2 flex justify-center items-center">
+                <EnvironmentOutlined className="text-[25px] opacity-70" />
+              </div>
+              <div className="ml-5">
+                <p className="font-bold">ADDRESS</p>
+                <p className="opacity-70">2727 Ocean Road, Malibu, CA, 90264</p>
               </div>
             </div>
-            <div className='flex space-x-2'>
-              <p className='font-bold'>Follow Us:</p>
-              <Image src="/path/to/facebook-icon.png" alt="Facebook" />
-              <Image src="/path/to/twitter-icon.png" alt="Twitter" />
-              <Image src="/path/to/linkedin-icon.png" alt="LinkedIn" />
-              <Image src="/path/to/youtube-icon.png" alt="YouTube" />
-              
+            <div className="flex flex-col space-x-2 md:ml-[15%]">
+              <p className="font-bold">Follow Us:</p>
+
+              <div className="flex mt-3">
+                <div className="h-[40px] w-[40px] ml-[-10px] rounded-[20px] border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700">
+                  <FacebookOutlined className="text-[20px] opacity-70 " />
+                </div>
+
+                <div className="h-[40px] w-[40px] ml-2 rounded-[20px] border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700">
+                  <WhatsAppOutlined className="text-[20px] opacity-70 " />
+                </div>
+
+                <div className="h-[40px] w-[40px] ml-2 rounded-[20px] border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700">
+                  <YoutubeOutlined className="text-[20px] opacity-70 " />
+                </div>
+
+                <div className="h-[40px] w-[40px] ml-2 rounded-[20px] border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700">
+                  <LinkedinOutlined className="text-[20px] opacity-70 " />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Contact;
