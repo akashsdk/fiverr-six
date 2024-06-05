e009a940e3c09f3a7beccebb81a21bcf9e0ae6e6
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-05 17:13:36 +0600

    ok

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,13 +13,13 @@ import {
 export default function Footer() {
   return (
     <div className="w-full relative">
-      <div className="w-full flex justify-center border-t-2">
+      <div className="web-bg w-full flex justify-center border-t-2">
         <div className="w-11/12 md:flex mt-5 mb-5">
           <div className="flex-1">jhbbhvvdhbv</div>
 
           <div className="flex-1 border-l-2 border-green-700">
             <p className="md:text-[30px] md:ml-3 text-[20px] font-bold mb-4 text-green-700">
-              Talk To Us
+              Stay Connected
             </p>
             <div className="ml-5">
               <p className="font-bold">EMAIL</p>
@@ -37,7 +37,58 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="flex-1">dd</div>
+          <div className="flex-1">
+            <p className="md:text-[30px] md:ml-3 text-[20px] font-bold mb-4 text-green-700">
+              Support
+            </p>
+
+            <a
+              className=" h-[60px] w-[230px] border-2 border-green-700 flex items-center justify-evenly rounded-[30px] opacity-60 hover:opacity-100"
+              href="tel:202-555-0188"
+            >
+              <PhoneOutlined className="transform rotate-90 text-green-700 font-bold text-[25px]" />
+              <div className="h-[30px] w-[2px] bg-green-700" />
+              <p className="font-bold text-[18px]">01321-100152</p>
+            </a>
+
+            <div className="flex mt-7">
+              <a
+                className="h-[50px] w-[50px] rounded-[5px] border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700"
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FacebookOutlined className="text-[30px] opacity-70 " />
+              </a>
+
+              <a
+                className="h-[50px] w-[50px] rounded-[5px]  ml-2  border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700"
+                href="https://www.whatsapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <WhatsAppOutlined className="text-[30px] opacity-70 " />
+              </a>
+
+              <a
+                className="h-[50px] w-[50px] rounded-[5px]  ml-2  border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700"
+                href="https://www.youtube.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <YoutubeOutlined className="text-[30px] opacity-70 " />
+              </a>
+
+              <a
+                className="h-[50px] w-[50px] rounded-[5px] ml-2 border-2 flex justify-center items-center border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedinOutlined className="text-[30px] opacity-70 " />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
